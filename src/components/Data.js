f04168875b08ec0ba3e435a10f75b96317db1721
Vichaslav Kostianets <vyacheslav.kostyanets@gmail.com>
2022-05-13 15:39:13 +0300

    added scss styles for filters

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,172 +1,197 @@
 export const data = [
 	{
 		id: 1,
-		name: 'Oil Oil for body 200ml Oil for body 200ml Oil for body 200ml',
+		name: 'Oil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/sHs7XQ7/images.jpg'
+		image: 'https://i.ibb.co/sHs7XQ7/images.jpg',
+		option: ['oil', 'yellow']
 	},
 	{
 		id: 2,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/GsKXZXr/33333333333333333333333333333333333333333333.jpg'
+		image: 'https://i.ibb.co/GsKXZXr/33333333333333333333333333333333333333333333.jpg',
+		option: ['scrub', 'blue']
 	},
 	{
 		id: 3,
-		name: 'Oil big Botlle',
+		name: 'Crema',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/02HbvVh/2222222222222222222222222222222222222222222.jpg'
+		image: 'https://i.ibb.co/02HbvVh/2222222222222222222222222222222222222222222.jpg',
+		option: ['cream', 'red']
 	}, {
 		id: 4,
-		name: 'Oil',
+		name: 'Crema',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/Jy69Cc6/0000000000000000000000.webp'
+		image: 'https://i.ibb.co/Jy69Cc6/0000000000000000000000.webp',
+		option: ['cream', 'blue']
 	},
 	{
 		id: 5,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/xz24TSL/999999999999999.jpg'
+		image: 'https://i.ibb.co/xz24TSL/999999999999999.jpg',
+		option: ['scrub', 'blue']
 	},
 	{
 		id: 6,
-		name: 'Oil big Botlle',
+		name: 'Bottle for oil',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/xfvdtSB/8888888888888.webp'
+		image: 'https://i.ibb.co/xfvdtSB/8888888888888.webp',
+		option: ['bottle', 'red']
 	}, {
 		id: 7,
 		name: 'Oil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/y5nTcHb/77777777777.jpg'
+		image: 'https://i.ibb.co/y5nTcHb/77777777777.jpg',
+		option: ['oil', 'green']
 	},
 	{
 		id: 8,
-		name: 'Scrub',
+		name: 'Scrub for leg',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/3RKqMdm/666666666666.webp'
+		image: 'https://i.ibb.co/3RKqMdm/666666666666.webp',
+		option: ['scrub', 'blue']
 	},
 	{
 		id: 9,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/FWpqyFP/5555555555555.webp'
+		image: 'https://i.ibb.co/FWpqyFP/5555555555555.webp',
+		option: ['scrub', 'red']
 	}, {
 		id: 10,
 		name: 'Oil for body 200ml Oil for body 200mlOil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/HtYdnb6/444444444444.jpg'
+		image: 'https://i.ibb.co/HtYdnb6/444444444444.jpg',
+		option: ['oil', 'pink']
 	},
 	{
 		id: 11,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/hVrm8KP/3333333333.jpg'
+		image: 'https://i.ibb.co/hVrm8KP/3333333333.jpg',
+		option: ['scrub', 'blue']
 	},
 	{
 		id: 12,
-		name: 'Oil big Botlle',
+		name: 'Oil Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/B3djLRg/2222222222222.jpg'
+		image: 'https://i.ibb.co/B3djLRg/2222222222222.jpg',
+		option: ['bottle', 'green']
 	},
 	{
 		id: 13,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/2sgVGcy/1111111111.jpg'
+		image: 'https://i.ibb.co/2sgVGcy/1111111111.jpg',
+		option: ['oil', 'red']
 	},
 	{
 		id: 13,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/rwf9DQy/11111111111111111111111111111111111111.webp'
+		image: 'https://i.ibb.co/rwf9DQy/11111111111111111111111111111111111111.webp',
+		option: ['bottle', 'yellow']
 	}, {
 		id: 14,
 		name: 'Oil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/YNnptND/10.jpg'
+		image: 'https://i.ibb.co/YNnptND/10.jpg',
+		option: ['oil', 'pink']
 	},
 	{
 		id: 15,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/Rv9Pkkx/9.jpg'
+		image: 'https://i.ibb.co/Rv9Pkkx/9.jpg',
+		option: ['scrub', 'red']
 	},
 	{
 		id: 16,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/Sd20VX2/8.jpg'
+		image: 'https://i.ibb.co/Sd20VX2/8.jpg',
+		option: ['oil', 'blue']
 	}, {
 		id: 17,
 		name: 'Oil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/dtpYzjX/7.jpg'
+		image: 'https://i.ibb.co/dtpYzjX/7.jpg',
+		option: ['oil', 'green']
 	},
 	{
 		id: 18,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/Ry62hd0/6.webp'
+		image: 'https://i.ibb.co/Ry62hd0/6.webp',
+		option: ['scrub', 'yellow']
 	},
 	{
 		id: 19,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/34rrkh8/5.jpg'
+		image: 'https://i.ibb.co/34rrkh8/5.jpg',
+		option: ['oil', 'blue']
 	}, {
 		id: 20,
 		name: 'Oil',
 		desc: 'Oil for body 200ml',
 		price: '$20',
-		image: 'https://i.ibb.co/HtYdnb6/444444444444.jpg'
+		image: 'https://i.ibb.co/HtYdnb6/444444444444.jpg',
+		option: ['oil', 'blue']
 	},
 	{
 		id: 21,
 		name: 'Scrub',
 		desc: 'Oil for skin',
 		price: '$10',
-		image: 'https://i.ibb.co/17H7M1B/4.jpg'
+		image: 'https://i.ibb.co/17H7M1B/4.jpg',
+		option: ['scrub', 'red']
 	},
 	{
 		id: 22,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/r4Rbfsd/3.jpg'
+		image: 'https://i.ibb.co/r4Rbfsd/3.jpg',
+		option: ['bottle', 'pink']
 	},
 	{
 		id: 23,
 		name: 'Oil big Botlle',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/8dhLFVY/2.jpg'
+		image: 'https://i.ibb.co/8dhLFVY/2.jpg',
+		option: ['oil', 'water']
 	},
 	{
 		id: 24,
-		name: 'Oil big Botlle',
+		name: 'scrub',
 		desc: 'Oil for body 500ml',
 		price: '$50',
-		image: 'https://i.ibb.co/1m0sKRv/1.webp'
+		image: 'https://i.ibb.co/1m0sKRv/1.webp',
+		option: ['scrub', 'water']
 	},
 ];
 
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,29 +1,66 @@
+import { Checkbox } from "@mui/material";
 import React from "react";
 
 const Filters = () => {
   return (
-    <ul className="main__filter__items">
-      <li className="main__filter__item">
-        <a href="" className="main__filter__link">
-          1 main__filter__link main__filter__link main__filter__link
-        </a>
-      </li>
-      <li className="main__filter__item">
-        <a href="" className="main__filter__link">
-          2 main__filter__link main__filter__link main__filter__link
-        </a>
-      </li>
-      <li className="main__filter__item">
-        <a href="" className="main__filter__link">
-          3 main__filter__linkmain__filter__link main__filter__link
-        </a>
-      </li>
-      <li className="main__filter__item">
-        <a href="" className="main__filter__link">
-          4
-        </a>
-      </li>
-    </ul>
+    <div className="main-filter">
+      <div className="filter__wrapper">
+        <h2 className="main-filter__header">Filter by color</h2>
+        <ul className="main-filter__items">
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              red
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              blue
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              yellow
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              pink
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="filter__wrapper">
+        <h2 className="main-filter__header">Filter by type</h2>
+        <ul className="main-filter__items">
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              1 main-filter__link main-filter__link main-filter__link
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              2 main-filter__link main-filter__link main-filter__link
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <a href="" className="main-filter__link">
+              3 main-filter__linkmain-filter__link main-filter__link
+            </a>
+          </li>
+          <li className="main-filter__item">
+            <Checkbox />
+            <div>4</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
